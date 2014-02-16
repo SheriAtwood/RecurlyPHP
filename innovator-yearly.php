0d8fb433f0ab7039404e5c1c4f86466d48271819
SupportPay <sheri.atwood@supportpay.com>
2014-02-16 02:22:06 -0800

    updated php files

--- innovator-yearly.php
+++ innovator-yearly.php
@@ -1,44 +1,46 @@
 <?php
-require_once ('lib/recurly.php');
+require_once('lib/recurly.php');
+ 
 // Required for the API
 Recurly_Client::$subdomain = 'supportpay';
 Recurly_Client::$apiKey = '3d301163be884aa5935baa1b76bc50a7';
 Recurly_js::$privateKey = 'a26316e92322494689c59a1d65b72e5b';
-$planCode = 'innovator';
+$planCode = 'innovator-yearly';
 $currency = 'USD';
 $signature = Recurly_js::sign(array($planCode));     
 ?>
-
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Innovator Monthly Subscription</title>
-<link rel="stylesheet" href="css/recurly.css" type="text/css" />
-<script src="/js/jquery.js"></script>
-<script src="/js/recurly.js"></script>
+<title>Innovator Yearly Subscription</title>
+<html>
+  <head>
+  <link rel="stylesheet" href="css/recurly.css" type="text/css" />
+<script src="js/jquery.js"></script>
+<script src="js/recurly.js"></script>
+    <script>
+    $(function(){
+      Recurly.config({
+        subdomain: 'supportpay'
+        , currency: 'USD' // GBP | CAD | EUR, etc...
+      });
 
-<script>
-
-  Recurly.config({
-    subdomain: 'supportpay',
-    currency: 'USD'
-  });
-  Recurly.buildSubscriptionForm({
-    target: '#recurly-subscribe',  
-    signature: '<?php echo $signature;?>',
-    successURL: 'confirmation.html',
-    planCode: 'innovator',
-    distinguishContactFromBillingInfo: false,
+      Recurly.buildSubscriptionForm({
+        target: '#recurly-subscribe',
+        planCode: 'innovator-yearly',
+        successURL: 'success.php',
+        signature: '<?php echo $signature;?>',
+distinguishContactFromBillingInfo: false,
     collectCompany: false,
     collectContact: true,
     termsOfServiceURL: 'http://supportpay.com/contact/terms-of-use/',
-	privacyPolicyURL: 'http://supportpay.com/contact/privacy-policy/',
+privacyPolicyURL: 'http://supportpay.com/contact/privacy-policy/',
     acceptPaypal: false,
     acceptedCards: ['mastercard',
                     'discover',
                     'american_express',
                     'visa'],
-	account: {
+account: {
 	    firstName: ''
 	  , lastName: ''
 	  , email: ''
@@ -61,7 +63,7 @@ $signature = Recurly_js::sign(array($planCode));
 </head>
   <body>
     <h1>Subscribe to Plan</h1>
-    <h2>Subscribe Test3</h2>
+    <h2>Plan Code: Innovator - Yearly4</h2>
     <div id="recurly-subscribe">
     </div>
   </body>
